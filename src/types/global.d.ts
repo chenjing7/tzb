cdd54ad3ce54f586175b3cf1a47039e8443a8526
chenjing7 <1015049570@qq.com>
2025-05-12 11:23:32 +0800

    refactor: simplify MarkdownContent component by removing HoverChart processing logic; update package dependencies and enhance table modal functionality for better user experience

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,9 +2,4 @@
 interface Window {
   // Function defined in MarkdownContent.astro
   processHoverCharts: () => void;
-  
-  // Global libraries
-  tippy: any;
-  Chart: any;
-  ChartDataLabels: any;
 } 
